120297d8f937a9677eb43bb2df8b026a0e1f0a51
acer <acer@192.168.0.100>
2022-12-12 15:48:30 +0530

    by using functional interface

---
+++ JunitPracice/src/Practice/Lambda.java
@@ -0,0 +1,23 @@
+package Practice;
+import java.util.Scanner;
+@FunctionalInterface
+interface Operator{
+	public void operate(int num);
+}
+
+public class Lambda {
+	public static void main(String args[]) {
+		int n;
+		System.out.println("enter the number");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		Operator op=num->System.out.println("Increasing the number by 2:"+(num+2));
+		op.operate(n); 
+	}
+
+
+
+}
+
+
+
