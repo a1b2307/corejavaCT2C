451a7b3d1ebffa8abfc25c8439b41e4f413f060c
acer <acer@192.168.0.103>
2022-12-13 12:16:53 +0530

    JUnit test

---
+++ JUnitPractice/src/JUnitCodes/TestJunit.java
@@ -0,0 +1,17 @@
+package JUnitCodes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class TestJunit {
+	
+    @Test
+	public void testsetup() {
+    	String name="I am done with Junit setup ";
+    	assertEquals("I am done with Junit setup ",name);
+    	
+	}
+
+}
