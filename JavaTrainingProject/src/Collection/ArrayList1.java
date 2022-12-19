0a78f64caf117d48491f502650b216926db69774
acer <acer@192.168.0.105>
2022-12-19 13:56:41 +0530

    Basic Example of List Interface In Collection API

---
+++ JavaTrainingProject/src/Collection/ArrayList1.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList1 {                   //This Example Shows you can Store any Type of Element in Collection
+
+	public static void main(String[] args) {
+		List al=new ArrayList();
+		al.add("Hello");
+		al.add(123);
+		al.add(3.14);
+		System.out.println(al);
+		
+
+	}
+
+}
