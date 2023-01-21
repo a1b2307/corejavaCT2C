2374e0a60d722af85ad69a42123b88ac39ed4b54
acer <acer@192.168.0.104>
2023-01-21 12:33:22 +0530

    main class

---
+++ placementClass/src/main/java/com/example/placementClass/PlacementClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.placementClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementClassApplication.class, args);
+	}
+
+}
